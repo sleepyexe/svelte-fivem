b4c99df0dc2e85fae2b8d50bf19ffb6485add189
Sleepy Rae <67649181+sleepyexe@users.noreply.github.com>
2024-11-15 20:59:47 +0700

    chore(main): Add background for debugging

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,6 +1,17 @@
 import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
+import { isEnvBrowser } from '$utils/misc'
+
+if (isEnvBrowser()) {
+  const root = document.getElementById('app');
+  root!.style.height = '100vh';
+  // root!.style.backgroundImage = 'url("background/night.png")';
+  root!.style.backgroundImage = 'url("background/day.png")';
+  root!.style.backgroundSize = 'cover';
+  root!.style.backgroundRepeat = 'no-repeat';
+  root!.style.backgroundPosition = 'center';
+}
 
 const app = mount(App, {
   target: document.getElementById('app')!,
